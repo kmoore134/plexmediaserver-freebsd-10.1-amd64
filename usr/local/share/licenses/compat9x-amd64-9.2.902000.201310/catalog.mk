e134958ada9cfefbac709fe656e022fc5d6c6a2b
Kris Moore <kris@pcbsd.org>
2015-07-23 11:16:43 -0400

    Add Plex Media Server packages

---
+++ usr/local/share/licenses/compat9x-amd64-9.2.902000.201310/catalog.mk
@@ -0,0 +1,5 @@
+_LICENSE=BSD2CLAUSE
+_LICENSE_NAME=BSD 2-clause Simplified License
+_LICENSE_PERMS=dist-mirror dist-sell pkg-mirror pkg-sell auto-accept
+_LICENSE_GROUPS=FSF OSI COPYFREE
+_LICENSE_DISTFILES=compat9x-amd64-9.2.902000.201310.tar.xz
